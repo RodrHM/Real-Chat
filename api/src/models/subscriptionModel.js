23272d31406547e66718c3e94b18af4ebca72646
Your Name <you@example.com>
2025-01-07 22:56:38 -0300

    Subscription Model

---
+++ api/src/models/subscriptionModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const subscriptionSchema = mongoose.Schema({
+    user_id:{type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+    server_id:{type: mongoose.Schema.Types.ObjectId, ref: 'server'},
+    chatRoom_id:{type: mongoose.Schema.Types.ObjectId, ref: 'chatRoom'},
+    role:{type: String },
+    createdAt:{ type: Date, default: Date.now}
+})
+
+const subscriptionModel = mongoose.model('subscription', subscriptionSchema)
+
+module.exports = subscriptionModel
