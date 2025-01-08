82db6f75b7dbe44a148aec44afafef883cefc4d7
Your Name <you@example.com>
2025-01-07 23:10:51 -0300

    ChatRoom Model

---
+++ api/src/models/chatRoomModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const chatRoomSchema = mongoose.Schema({
+    name:{type: String, required: true},
+    description:{type: String},
+    users_id:[{type: mongoose.Schema.Types.ObjectId, ref:'user'}],
+    createdAt: { type: Date, default: Date.now }, updatedAt: { type: Date, default: Date.now}
+},{
+    timestamps: true
+})
+
+const ChatRoomModel = mongoose.model('chatRoom', chatRoomSchema)
+
+module.exports = ChatRoomModel
